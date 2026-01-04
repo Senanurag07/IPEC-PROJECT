256e11d5f4d72cf7264c99110649fd5ef8b1a4ea
Senanurag07 <senanurag061@gmail.com>
2026-01-05 01:19:24 +0530

    Add initial implementation of page components and services section

---
+++ app/Premier-Support/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import HeroSection from '../components/HeroSectiondynamic'
+import PrimaveraSupportServices from '../components/PrimaveraSupportServices'
+import WhyChooseUs from '../components/WhyChooseUs'
+import ContactSection from '../components/ContactSection'
+
+function page() {
+  return (
+    <main>
+      <HeroSection 
+      title="Primavera Premier Support"
+      subtitle='Your trusted Oracle Primavera support partner delivering end-to-end technical and functional support for project-driven organizations.'
+      bgImage='/assests/hero1.jpg'
+      />
+      <PrimaveraSupportServices />
+      <WhyChooseUs />
+      <ContactSection />
+    </main>
+  )
+}
+
+export default page
