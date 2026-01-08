ccba92827cf89375906f11b581046c8e14d254a0
Senanurag07 <senanurag061@gmail.com>
2026-01-08 23:32:21 +0530

    photo tagging

--- app/components/WhoWeWorkWith.tsx
+++ app/components/WhoWeWorkWith.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const partners = [
   {
     title: "Construction & Engineering",
-    image: "/assests/property.jpeg",
+    image: "/assests/gov.jpeg",
   },
   {
     title: "Oil & Gas",
